5c65c3e93404e2781277957e976e0e512dc53cda
Teddy Yongo <teyddie@gmail.com>
2024-12-20 13:40:32 +0300

    Feat: adding Auth provider to app

--- src/App.jsx
+++ src/App.jsx
@@ -5,35 +5,39 @@ import Home from "./pages/Home/Home";
 import PageNotFound from "./pages/PageNotFound";
 import AppLayout from "./pages/AppLayout/AppLayout";
 import Login from "./pages/Login/Login";
-import CityList from './components/City/CityList';
-import CountryList from './components/Country/CountryList';
-import City from './components/City/City';
-import Form from './components/Form/Form'
+import CityList from "./components/City/CityList";
+import CountryList from "./components/Country/CountryList";
+import City from "./components/City/City";
+import Form from "./components/Form/Form";
 
-import { CitiesProvider} from './context/CitiesContext';
-
-
+import { CitiesProvider } from "./context/CitiesContext";
+import { AuthProvider } from "./context/AuthContext";
 
 function App() {
 	return (
-		<CitiesProvider>
-			<BrowserRouter>
-				<Routes>
-					<Route index element={<Home />} />
-					<Route path='pricing' element={<Pricing />} />
-					<Route path='product' element={<Product />} />
-					<Route path='login' element={<Login />} />
-					<Route path='app' element={<AppLayout />}>
-						<Route index element={<Navigate replace to='cities'/>} />
-						<Route path='cities' element={<CityList/>} />
-						<Route path='cities/:id' element={<City/>}/>
-						<Route path='countries' element={<CountryList/>} />
-						<Route path='form' element={<Form/>} />
-					</Route>
-					<Route path='*' element={<PageNotFound />} />
-				</Routes>
-			</BrowserRouter>
-		</CitiesProvider>
+		<AuthProvider>
+			<CitiesProvider>
+				<BrowserRouter>
+					<Routes>
+						<Route index element={<Home />} />
+						<Route path="pricing" element={<Pricing />} />
+						<Route path="product" element={<Product />} />
+						<Route path="login" element={<Login />} />
+						<Route path="app" element={<AppLayout />}>
+							<Route
+								index
+								element={<Navigate replace to="cities" />}
+							/>
+							<Route path="cities" element={<CityList />} />
+							<Route path="cities/:id" element={<City />} />
+							<Route path="countries" element={<CountryList />} />
+							<Route path="form" element={<Form />} />
+						</Route>
+						<Route path="*" element={<PageNotFound />} />
+					</Routes>
+				</BrowserRouter>
+			</CitiesProvider>
+		</AuthProvider>
 	);
 }
 
