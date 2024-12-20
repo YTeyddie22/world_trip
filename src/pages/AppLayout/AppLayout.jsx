8fb58ecb5bc74c4ce4680e830d725613ab478fb1
Teddy Yongo <teyddie@gmail.com>
2024-12-20 14:14:47 +0300

    add User component

--- src/pages/AppLayout/AppLayout.jsx
+++ src/pages/AppLayout/AppLayout.jsx
@@ -2,12 +2,14 @@
 import SideBar from "../../components/Sidebar/SideBar";
 import Map from "../../components/Map/Map";
 import styles from "./AppLayout.module.css";
+import User from "../../components/User/User";
 
 function AppLayout() {
 	return (
 		<div className={styles.app}>
 			<SideBar />
 			<Map />
+			<User />
 		</div>
 	);
 }
