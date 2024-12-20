fa8ec6c40346f4c196412a627ed2d2349a9439a9
Teddy Yongo <teyddie@gmail.com>
2024-12-20 14:09:50 +0300

    validating the auth in page

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,38 +1,63 @@
+import { useNavigate } from "react-router-dom";
+import Button from "../../components/Button/Button";
 import PageNav from "../../components/PageNav/PageNav";
+import { useAuth } from "../../context/AuthContext";
 import styles from "./Login.module.css";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export default function Login() {
 	// PRE-FILL FOR DEV PURPOSES
+
+	const { login, isAuthenticated } = useAuth();
+
 	const [email, setEmail] = useState("jack@example.com");
 	const [password, setPassword] = useState("qwerty");
+	const navigate = useNavigate();
+
+	useEffect(
+		function () {
+			if (isAuthenticated)
+				navigate("/app", {
+					replace: true,
+				});
+		},
+		[isAuthenticated, navigate]
+	);
+
+	function handleSubmit(e) {
+		e.preventDefault();
+
+		if (email && password) {
+			login(email, password);
+		}
+	}
 
 	return (
 		<main className={styles.login}>
 			<PageNav />
-			<form className={styles.form}>
+			<form className={styles.form} onSubmit={handleSubmit}>
 				<div className={styles.row}>
-					<label htmlFor='email'>Email address</label>
+					<label htmlFor="email">Email address</label>
 					<input
-						type='email'
-						id='email'
+						type="email"
+						id="email"
 						onChange={(e) => setEmail(e.target.value)}
 						value={email}
 					/>
 				</div>
 
 				<div className={styles.row}>
-					<label htmlFor='password'>Password</label>
+					<label htmlFor="password">Password</label>
 					<input
-						type='password'
-						id='password'
+						type="password"
+						id="password"
 						onChange={(e) => setPassword(e.target.value)}
 						value={password}
 					/>
 				</div>
 
 				<div>
-					<button>Login</button>
+					<Button type="primary">Login</Button>
 				</div>
 			</form>
 		</main>
